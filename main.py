65ff83cfdb478c4b6d9f153ba3b49e8ade3eaa32
James Winston <kenjameswinston@gmail.com>
2022-05-12 09:15:06 -0400

    Tweak UI colors to change with timer state

--- main.py
+++ main.py
@@ -14,53 +14,51 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+REPS = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    # Clear display
-    canvas.delete(ALL)
-    canvas.create_text(103, 130, fill="white", text="25:00", font=(FONT_NAME, 35, "bold"))
+    global REPS
+    REPS = 0
+    canvas.itemconfig(timer_display, text=f"00:00")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    minutes = 25
-    seconds = minutes * 60
-    countdown(seconds)
+    global REPS
+    REPS += 1
+
+    if REPS % 8 == 0:
+        timer_label.config(text="Break", fg=BEIGE)
+        countdown(LONG_BREAK_MIN * 60)
+    elif REPS % 2 == 0:
+        timer_label.config(text="Break", fg=GREEN)
+        countdown(SHORT_BREAK_MIN * 60)
+    else:
+        timer_label.config(text="Working", fg=MAROON)
+        countdown(WORK_MIN * 60)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
 def countdown(count):
-
     # -----REFACTOR----- #
     minutes = count // 60
     seconds = count % 60
+    # To display :00 correctly
+    if seconds < 10:
+        seconds = f"0{seconds}"
     canvas.itemconfig(timer_display, text=f"{minutes}:{seconds}")
     if count > 0:
         window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
 
-
-
-    # For each minute, count 60 seconds and subtract a minute
-    # while minutes > 0:
-    #     seconds = 60
-    #     minutes -= 1
-    #     while seconds > 0:
-    #         seconds -= 1
-    #         # if less than 10 seconds, display the extra 0 ie; 00:09
-    #         if seconds < 10:
-    #             timer_label_1 = canvas.create_text(103, 130, text=f"{minutes}:0{seconds}", fill="white", font=(FONT_NAME, 35, "bold"))
-    #             # Clear canvas
-    #             canvas.update()
-    #             canvas.delete(timer_label_1)
-    #         else:
-    #             timer_label_2 = canvas.create_text(103, 130, text=f"{minutes}:{seconds}", fill="white", font=(FONT_NAME, 35, "bold"))
-    #             # Clear canvas
-    #             canvas.update()
-    #             canvas.delete(timer_label_2)
-            # We use this to count our second
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Draw our window
@@ -69,13 +67,13 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=BLUE)
 
 # Create tomato background
-canvas = Canvas(width=210, height=224, bg=BLUE, highlightthickness=0)
+canvas = Canvas(width=250, height=250, bg=BLUE, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(103, 112, image=tomato_img)
 canvas.grid(row=1, column=1)
 
 # Display countdown timer
-timer_display = canvas.create_text(103, 130, text="25:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_display = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 # Draw 'Timer' label
 timer_label = Label(text="Timer", font=(FONT_NAME, 36))
@@ -94,6 +92,5 @@ reset_button.grid(row=2, column=2)
 # Draw checkmarks
 check_mark = Label(text="✔", fg=MAROON, bg=BLUE, font=("courier", 33, "bold"))
 check_mark.grid(row=3, column=1)
-
 
 window.mainloop()
